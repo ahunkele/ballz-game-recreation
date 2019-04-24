173381dbcd8cb3038fe3ee269fb56d89625395a6
ahunkele <31667935+ahunkele@users.noreply.github.com>
2019-04-24 16:43:59 -0500

    Ball and Ball Launcher Creation
    created a ball GameObject and A BallLauncher Script which launches a spawns and launches the ball in the given direction.

---
+++ Ballz_Game/Assets/Scrips/BallLauncher.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallLauncher : MonoBehaviour
+{
+    private Vector3 startPosition;
+    private Vector3 endPosition;
+
+    [SerializeField]
+    private GameObject ballPrefab;
+
+    private void Update()
+    {
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) + Vector3.back * -10;
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            StratDrag(worldPosition);
+        }
+        else if(Input.GetMouseButton(0))
+        {
+            ContinueDrag(worldPosition);
+        }
+        else if(Input.GetMouseButtonUp(0))
+        {
+            EndDrag();
+        }
+        
+    }
+
+    private void EndDrag()
+    {
+        Vector3 direction = endPosition - startPosition;
+        direction.Normalize();
+
+        var ball = Instantiate(ballPrefab, transform.position, Quaternion.identity);
+        ball.GetComponent<Rigidbody2D>().AddForce(-direction);
+    }
+
+    private void ContinueDrag(Vector3 worldPosition)
+    {
+        endPosition = worldPosition;
+    }
+
+    private void StratDrag(Vector3 worldPosition)
+    {
+        startPosition = worldPosition;
+    }
+}
